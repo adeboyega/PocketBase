d8c8261095495cdda0ae76cd5f6badbd1a669a87
Bayomi Aremo <8955yomi@gmail.com>
2024-06-14 14:15:49 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./components/Home";
+function App() {
+
+	return (
+		<div>
+      <Home />
+    </div>
+	);
+}
+
+export default App;
